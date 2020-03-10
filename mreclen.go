7a06342e1b02dac0747056c0559bcb357e51b57f
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-03-10 14:32:23 +0900

    Fix a reported message and test case

--- mreclen.go
+++ mreclen.go
@@ -24,14 +24,12 @@ const (
 func run(pass *analysis.Pass) (interface{}, error) {
 	for _, f := range pass.Files {
 		for _, d := range f.Decls {
-			// prints x to stdout for debug
-			ast.Print(nil, d)
 			if v, ok := d.(*ast.FuncDecl); ok {
 				// verify v.Recv if not nil
 				if v.Recv != nil {
 					r := v.Recv.List[0].Names[0].Name
 					if len(r) > defMaxLen {
-						pass.Reportf(v.Pos(), "method's receiver(%s) length overs max length(%d)", r, defMaxLen)
+						pass.Reportf(v.Pos(), "%s should be a one or two letter", r)
 					}
 				}
 			}
--- testdata/src/sample/sample.go
+++ testdata/src/sample/sample.go
@@ -1,11 +1,11 @@
 package sample
 
-func dummyFunc() {}
+func dummyFunc() {} // OK
 
-func is(s string, i int) bool { return false }
+func is(s string, i int) bool { return false } // OK
 
 type client struct{}
 
-func (c *client) dummyMethod() {}
+func (c *client) dummyMethod() {} // OK
 
-func (cli *client) dummyMethod2() {}
+func (cli *client) dummyMethod2() {} // want "cli should be a one or two letter"
