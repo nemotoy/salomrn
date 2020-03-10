a14a61ce264c0062a035483435ff636784a0d874
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-03-10 10:53:32 +0900

    Add sample testdata

---
+++ mreclen_test.go
@@ -0,0 +1,13 @@
+package mreclen_test
+
+import (
+	"testing"
+
+	"github.com/nemotoy/mreclen"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+func Test(t *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.Run(t, testdata, mreclen.Analyzer, "sample")
+}
