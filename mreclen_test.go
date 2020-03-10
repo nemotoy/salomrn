3259631677c8b29a104aa7685a2743ba7dba74a0
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-03-10 14:33:18 +0900

    Rename files

--- mreclen_test.go
+++ mreclen_test.go
@@ -9,5 +9,5 @@ import (
 
 func Test(t *testing.T) {
 	testdata := analysistest.TestData()
-	analysistest.Run(t, testdata, mreclen.Analyzer, "sample")
+	analysistest.Run(t, testdata, mreclen.Analyzer, "a")
 }
